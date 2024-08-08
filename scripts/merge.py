21870275ceeb6e1e766826fa1ce2eaed0b31284a
Fahmi Fisal <ryuzaki_l@y7mail.com>
2024-08-08 17:28:39 +0800

    merge plalist

--- scripts/merge.py
+++ scripts/merge.py
@@ -14,6 +14,6 @@ def merge_m3u(files, output_file):
     print(f"Merged playlists saved to {output_file}.")
 
 # Example usage after your existing M3U generation code
-files_to_merge = ['playlist.m3u','mylist.m3u8','1.AstroIPTV.m3u','6.other.m3u8', '7.hello.m3u8','2.raku.m3u8', '0.tvseries.m3u']
+files_to_merge = ['playlist.m3u','mylist.m3u8','1.AstroIPTV.m3u','6.other.m3u8','Playlist6.m3u', '7.hello.m3u8','2.raku.m3u8', '0.tvseries.m3u']
 output_filename = 'merged_playlist.m3u'
 merge_m3u(files_to_merge, output_filename)
